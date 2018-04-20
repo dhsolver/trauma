92b4f253ed2262f65e5b48117315c1576a51147d
dan1114ho <dan1114ho@gmail.com>
2018-04-20 22:22:03 +0200

    added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title') 404 :: @parent @endsection
+@section('content')
+    <div class="row">
+        <div class="page-header">
+            <h2>Page not found</h2>
+        </div>
+    </div>
+@endsection
