e1ef5437ee55976903be7f9b9df38ccd6eac06f4
dan1114ho <dan1114ho@gmail.com>
2018-04-22 15:15:00 +0800

    update menu style

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,16 +2,16 @@
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
+                    data-target="#main-navbar">
                 <span class="sr-only">Toggle Navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">@section('title')@show</a>
+            <a class="navbar-brand" href="#" data-toggle="collapse" data-target="#main-navbar">@section('title')@show</a>
         </div>
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse" id="main-navbar">
             <ul class="nav navbar-nav">
                 <li class="{{ (Request::is('/') ? 'active' : '') }}">
                     <a href="{{ url('') }}">Home</a>
