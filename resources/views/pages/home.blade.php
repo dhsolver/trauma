53e06b2616fd795e0a38a3d8b1412e17d12ce947
dan1114ho <dan1114ho@gmail.com>
2018-04-26 20:53:55 +0800

    add brochure pdf

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,7 +12,7 @@
         <div class="col-sm-4">
             <div class="content-box text-center">
                 <img src="/images/computer_w_coding.png" alt="computer coding screen" />
-                <h3 class="title">Specialized Injury Coding Solutions</h3>
+                <h3 class="title"><a href="/docs/TraumaInjuryCodingCourseBrochure.pdf">Specialized Injury Coding Solutions</a></h3>
             </div>
         </div>
         <div class="col-sm-4">
