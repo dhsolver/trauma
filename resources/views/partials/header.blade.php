2d515332da2f88b18fcb38de394e71e894eec5b7
dan1114ho <dan1114ho@gmail.com>
2018-05-02 17:21:47 +0800

    updated nav item color

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-offset-3 col-sm-6 col-xs-offset-1 col-xs-10">
-                <img class="logo" src="/images/trauma_analytics_logo.png" alt="trauma analytics logo"></a>
+                <a href="{{ url('/') }}"><img class="logo" src="/images/trauma_analytics_logo.png" alt="trauma analytics logo"></a>
             </div>
         </div>
     </div>
