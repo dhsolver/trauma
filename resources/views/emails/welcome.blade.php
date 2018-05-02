1e4a08051af7ac7095d720e7f30350d5eee12921
dan1114ho <dan1114ho@gmail.com>
2018-05-02 17:23:19 +0800

    fix welcome email copy typo

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,6 +1,6 @@
 <h2>Welcome to Trauma Analytics</h2>
 Hi {{ $user->first_name }},<br />
 Your account is successfully registered and submitted for review. <br>
-It will be processed in will be processed within the next 24-48 hours.<br /><br />
+It will be processed within the next 24-48 hours.<br /><br />
 Thanks and stay tuned!<br />
 Trauma Analytics Team
