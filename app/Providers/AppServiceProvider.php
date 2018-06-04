ea5576ac68a58d353173543e7254bd4765a3204a
dan1114ho <dan1114ho@gmail.com>
2018-06-05 00:27:01 +0800

    support https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(env('REDIRECT_HTTPS'))
+        {
+            \URL::forceSchema('https');
+        }
     }
 
     /**
