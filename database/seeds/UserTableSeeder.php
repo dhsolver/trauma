b549de998fdc55d8f145b1352e7a8201ff9582c0
dan1114ho <dan1114ho@gmail.com>
2018-07-17 09:41:20 +0800

    fixed (seed): change `admin` to `role`

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -3,16 +3,16 @@
 use Illuminate\Database\Seeder;
 
 class UserTableSeeder extends Seeder {
-	public function run()
-	{
-		\App\User::create([
-			'first_name' => 'Admin',
-			'last_name' => 'User',
-			'email' => 'admin@admin.com',
-			'password' => bcrypt('admin'),
-			'confirmed' => 1,
-            'admin' => 1,
-			'confirmation_code' => md5(microtime() . env('APP_KEY')),
-		]);
-	}
+    public function run()
+    {
+        \App\User::create([
+            'first_name' => 'Admin',
+            'last_name' => 'User',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+            'confirmed' => 1,
+            'role' => 'admin',
+            'confirmation_code' => md5(microtime() . env('APP_KEY')),
+        ]);
+    }
 }
