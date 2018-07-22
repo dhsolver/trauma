526b73400cf7fa36af4b4a2bbf68557bcf85e75e
dan1114ho <dan1114ho@gmail.com>
2018-07-22 13:26:18 +0800

    fix: approved email copy and a notification message

--- resources/views/emails/approved.blade.php
+++ resources/views/emails/approved.blade.php
@@ -1,7 +1,6 @@
 Hi {{ $user->first_name }},<br />
 Your Trauma Analytics profile has been approved!<br />
-The next step is to browse available courses at <a href="{{ url() }}">Trauma Analytics</a> and expand your knowledge.<br />
+The next step is to browse available courses at <a href="{{ url() }}">Trauma Analytics</a> and expand your knowledge.<br />.<br />
 Good luck with your learning.<br />
 <br />
-Thanks and stay tuned!<br />
 Trauma Analytics Team
