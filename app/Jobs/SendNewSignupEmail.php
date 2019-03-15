5c148a62b6ee879c933cf3e4f4f4c52064fbdbe9
dan1114ho <dan1114ho@gmail.com>
2019-03-16 04:25:06 +0800

    send out new signup email to admin

---
+++ app/Jobs/SendNewSignupEmail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\User;
+use App\Jobs\Job;
+use Illuminate\Contracts\Mail\Mailer;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendNewSignupEmail extends Job implements SelfHandling, ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    protected $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(Mailer $mailer)
+    {
+        $mailer->send('emails.new_student', ['user' => $this->user], function ($m) {
+            $m->to('admin@traumaanalytics.com')->subject('New Student Registration');
+        });
+    }
+}
---
+++ resources/views/emails/new_student.blade.php
@@ -0,0 +1,4 @@
+Hello admin,<br />
+A new student ({{ $user->email }}) has signed up for traumaanalytics.com. Please review and approve/reject.<br />
+<br />
+Cheers!
